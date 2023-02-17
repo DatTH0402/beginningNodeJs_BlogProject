1ff4c69842e444cde4f9d85b51b374ecf4105545
DATTH0402 <tranhuudat.0402.developer@gmail.com>
2023-02-17 15:08:09 +0700

    mvc refactoring

--- index.js
+++ index.js
@@ -1,73 +1,35 @@
 const express = require('express')
 const app = new express()
-const path = require('path')
+const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+
+const mongoose = require('mongoose');
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const newPostController = require('./controllers/newPost')
+const validateMiddleware = require("./middleware/validationMiddleware.js");
+
+app.set('view engine', 'ejs')
+mongoose.connect('mongodb://0.0.0.0:27017/test', { useNewUrlParser: true });
+mongoose.set('strictQuery', true);
 
 app.use(express.static('public'))
-
-const ejs = require('ejs')
-app.set('view engine', 'ejs')
-
-const bodyParser = require('body-parser')
+app.use('/posts/store',validateMiddleware)
+app.use(fileUpload())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const fileUpload = require('express-fileupload')
-app.use(fileUpload())
 
-//Custom Middleware
-const validateMiddleWare = (req, res, next) => {
-    if (req.files == null || req.body.title == null || req.body.title == null) {
-        return res.redirect('/posts/new')
-    }
-    next()
-}
-app.use('/posts/store',validateMiddleWare)
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/test', { useNewUrlParser: true });
-mongoose.set('strictQuery', true);
 
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
 
-const BlogPost = require('./models/BlogPost.js');
-
-app.get('/', async (req, res) => {
-    const blogposts = await BlogPost.find({})
-    res.render('index', {
-        blogposts
-    });
-})
-
-app.get('/about', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/about.html'))
-    res.render('about');
-})
-app.get('/contact', (req, res) => {
-    //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
-    res.render('contact');
-})
-
-app.get('/post/:id', async (req, res) => {
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post', {
-        blogpost
-    })
-})
-
-app.get('/posts/new', (req, res) => {
-    res.render('create')
-})
 
 
-app.post('/posts/store', (req, res) => {
-    let image = req.files.image;
-    image.mv(path.resolve(__dirname, 'public/img', image.name), async(error)=> {
-        await BlogPost.create({
-            ...req.body,
-            image: '/img/' + image.name
-        })
-        res.redirect('/')
-    })
-})
+app.get('/',homeController)
+app.get('/post/:id',getPostController)
+app.post('/posts/store', storePostController)
+app.get('/posts/new', newPostController)
