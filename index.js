702812c3941e156423b366043cc5bce1958d5bd0
DATTH0402 <tranhuudat.0402.developer@gmail.com>
2023-03-06 14:59:35 +0700

    user registration

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const homeController = require('./controllers/home')
 const storePostController = require('./controllers/storePost')
 const getPostController = require('./controllers/getPost')
 const newPostController = require('./controllers/newPost')
-const validateMiddleware = require("./middleware/validationMiddleware.js");
+const validateMiddleware = require("./middleware/validationMiddleware.js")
+const newUserController = require('./controllers/newUser')
+const storeUserController = require('./controllers/storeUser')
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
 
 app.set('view engine', 'ejs')
 mongoose.connect('mongodb://0.0.0.0:27017/test', { useNewUrlParser: true });
@@ -21,15 +25,15 @@ app.use(fileUpload())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
-
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
-
-
 
 app.get('/',homeController)
 app.get('/post/:id',getPostController)
 app.post('/posts/store', storePostController)
 app.get('/posts/new', newPostController)
+app.get('/auth/register', newUserController)
+app.post('/users/register', storeUserController)
+app.get('/auth/login', loginController)
+app.post('/users/login',loginUserController)
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+/*
+-   UserSchema.pre( ‘ save ’… , we tell Mongoose that before we save any record into the Users schema or Users collection, 
+    execute the function passed into the 2nd argument. This lets us change user data before saving it into the database.
+-   Note that we have to specify function(next) instead of using the lambda short form of a function i.e. next => {…} . 
+    I am not too sure why the lambda short form doesn ’ t work.
+-   mongoose makes the UserSchema available via this.
+-   We then proceed to call bcrypt.hash whose first argument takes in the password to be hashed. 
+    The second argument specifies the number of roundsof hashing to take place. 
+    For example, we have specified 10 which means that the password will be encrypted 10 times. 
+-   user.password = hash replaces the original password with its encrypted version.
+-   We then call next() so that mongoose can continue creating the user data.
+*/
+UserSchema.pre('save', function (next) {
+    const user = this
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
+
+// export model
+const User = mongoose.model('User', UserSchema);
+module.exports = User
